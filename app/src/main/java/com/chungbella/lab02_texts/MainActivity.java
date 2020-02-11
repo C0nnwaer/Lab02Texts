c16b4630c68363a18a15edcde671e23248df2b60
C0nnwaer <webellachung@gmail.com>
2020-02-11 09:01:36 -0500

    Print text entered by user when button pressed

--- app/src/main/java/com/chungbella/lab02_texts/MainActivity.java
+++ app/src/main/java/com/chungbella/lab02_texts/MainActivity.java
@@ -3,12 +3,29 @@ package com.chungbella.lab02_texts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button submit_button;
+    TextView name_display = findViewById(R.id.name_display);
+    EditText entered_name = findViewById(R.id.enter_name);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                @Override
+                String name = entered_name.getText();
+                Log.i("submit_button", "Name entered by user");
+            }
+        });
     }
 }
