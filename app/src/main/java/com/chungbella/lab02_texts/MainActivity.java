a0ff274a7e46d4a85cc7a4035c4055d3a385e0ee
C0nnwaer <webellachung@gmail.com>
2020-02-13 08:51:42 -0500

    Message increments

--- app/src/main/java/com/chungbella/lab02_texts/MainActivity.java
+++ app/src/main/java/com/chungbella/lab02_texts/MainActivity.java
@@ -2,6 +2,7 @@ package com.chungbella.lab02_texts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,7 +13,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button submit_button;
     TextView name_display;
+    TextView message;
     EditText entered_name;
+    int msg_index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
         submit_button = findViewById(R.id.edit_button);
         name_display = findViewById(R.id.name_display);
+        message = findViewById(R.id.message);
         entered_name = findViewById(R.id.enter_name);
+
+        Resources res = getResources();
+        final String[] messages = res.getStringArray(R.array.messages);
 
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 String name = entered_name.getText().toString().toUpperCase();
                 Log.i("submit_button", name);
                 name_display.setText(name);
+
+                String msg = messages[msg_index];
+                message.setText(msg);
+                msg_index += 1;
+                if(msg_index >= messages.length){
+                    msg_index = 0;
+                }
             }
         });
     }
